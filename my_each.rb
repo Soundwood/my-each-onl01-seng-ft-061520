94ce3b84e8206f1dcdac3f55096e582fd3970cd7
Ben Hogan <j.benhogan@gmail.com>
2020-06-18 23:22:08 +0000

    Automatically backed up by Learn

--- my_each.rb
+++ my_each.rb
@@ -2,5 +2,12 @@ collection = [1, 2, 3, 4]
 
 def my_each(array) # put argument(s) here
   # code here
-  puts i 
+  array_size = array.size
+  new_array = []
+  couter = 0 
+  while counter < array_size
+    new_array << array[counter]
+    counter += 1 
+  end
+  return new_array
 end
