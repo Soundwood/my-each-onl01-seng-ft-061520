d98f39e2b277f2a6bf2b24be8bc54e33a82d065c
Ben Hogan <j.benhogan@gmail.com>
2020-06-18 23:26:26 +0000

    Automatically backed up by Learn

--- my_each.rb
+++ my_each.rb
@@ -4,7 +4,7 @@ def my_each(array) # put argument(s) here
   # code here
   array_size = array.size
   new_array = []
-  couter = 0 
+  counter = 0 
   while counter < array_size
     new_array << array[counter]
     counter += 1 
